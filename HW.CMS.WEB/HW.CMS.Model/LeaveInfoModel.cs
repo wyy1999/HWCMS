eccf66df1bad86f7e9a9be0879f2f14be576c4f3
柳林军 <1169598336@qq.com>
2019-11-21 16:13:12 +0800

    modal

--- HW.CMS.WEB/HW.CMS.Model/LeaveInfoModel.cs
+++ HW.CMS.WEB/HW.CMS.Model/LeaveInfoModel.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace HW.CMS.Model
 {
-    class LeaveInfoModel
+    public class LeaveInfoModel
     {
-
+        public int LeaveId { get; set; }
+        public int InfoId { get; set; }
+        public int DepId { get; set; }
+        public string LeaveTime { get; set; }
+        public string LeaveReason { get; set; }
+        public int LeaveState { get; set; }
     }
 }
