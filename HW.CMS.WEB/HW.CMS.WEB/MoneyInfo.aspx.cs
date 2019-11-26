a613665cad3ceaf74e4494962515b59f79b1a0a5
zdh <2289304056@qq.com>
2019-11-26 21:04:19 +0800

    工资结算

---
+++ HW.CMS.WEB/HW.CMS.WEB/MoneyInfo.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HW.CMS.BLL;
+
+namespace HW.CMS.WEB
+{
+    public partial class MoneyInfo : System.Web.UI.Page
+    {
+        MoneyInfoBll bll = new MoneyInfoBll();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Repeater1_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            string name = e.CommandName;
+            int userid = Convert.ToInt32(e.CommandArgument);
+
+            if (name == "update")
+            {
+                bll.update(userid);
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string InfoName = TextBox1.Text;
+            int MoneyState = Convert.ToInt32(RadioButtonList1.SelectedValue);
+            int Moneysta = Convert.ToInt32(RadioButtonList1.SelectedValue);
+            Repeater1.DataSource = bll.setAll(InfoName,MoneyState, Moneysta);
+            Repeater1.DataBind();
+        }
+    }
+}
